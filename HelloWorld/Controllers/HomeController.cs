dca06ae10fb8c7dec922dc0208a4d278031d1888
XiangRn <cuadao1710@gmail.com>
2021-03-26 15:32:09 +0700

    Add OnlineShop

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using HelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Chào mừng Viên Viên đến với nhóm chúng tôi";
+            MessageModel message = new MessageModel();
+            message.Welcome = "Viên Viên";
+            return View(message);
+        }
+    }
+}
